4730bd52d539fbde83df52b43c1aab244642f21b
Q Visible <45730249+qVisible@users.noreply.github.com>
2022-03-21 21:35:15 +0000

    Add files via upload

--- nav.php
+++ nav.php
@@ -8,4 +8,5 @@ $currentFile = $_SERVER['PHP_SELF'];
 $parts = Explode('/', $currentFile);
 echo $parts[count($parts) - 1]; //Get the current filename and pass it to the Get Code Page
 ?>">code</a>
+<img src="Judith-logos_transparent.png">
 </nav>
